f4f4562fa01d4259f59e48405dfa2d6b777db7d4
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-08-13 20:29:45 -0700

    trying to authenticate

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template, jsonify, redirect
-# from flask_pymongo import PyMongo
+from flask import Flask, render_template, jsonify, redirect, session
+from authlib.flask.client import OAuth
+from flask_pymongo import PyMongo
+from functools import wraps
 import os
 
 app = Flask(__name__)
-# app.config["MONGO_URI"] = os.environ['MONGODB_URI']
-# mongo = PyMongo(app)
+app.config["MONGO_URI"] = os.environ['MONGODB_URI']
+mongo = PyMongo(app)
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=os.environ['AUTH_CLIENT'],
+    client_secret=os.environ['AUTH_SECRET'],
+    api_base_url='https://gigdb.auth0.com',
+    access_token_url='https://gigdb.auth0.com/oauth/token',
+    authorize_url='https://gigdb.auth0.com/authorize',
+    client_kwargs={
+        'scope': 'openid profile',
+    },
+)
+
+# Here we're using the /callback route.
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri='YOUR_CALLBACK_URL', audience='https://YOUR_AUTH0_DOMAIN/userinfo')
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
 
 @app.route("/")
 def home():
